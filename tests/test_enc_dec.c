5f6fbfdc77c3945c4a474b9666fe705cf30d4495
Chaiyawan A <ca1216@ic.ac.uk>
2021-08-10 00:50:28 +0100

    Bug

--- tests/test_enc_dec.c
+++ tests/test_enc_dec.c
@@ -15,7 +15,8 @@ uint8_t * gen_data(int width, int height, int nframes){
   return data;
 }
 
-int main(){
+int main()
+{
   int width = 1000;
   int height = 1000;
   int nframes = 200;
@@ -23,12 +24,13 @@ int main(){
   char * data = (char *)gen_data(width, height, nframes);
   size_t encoded_size;
   int data_sum = 0;
-  for(int i = 0;i<size;i++){
+  for(int i = 0;i < size;i++)
+  {
     data_sum += data[i];
   }
+
   int64_t tic = av_gettime();
-  char * encoded = h264_encode(data, size, height, width,
-			       1, &encoded_size);
+  char * encoded = h264_encode(data, size, height, width, 1, &encoded_size);
   int64_t toc = av_gettime();
   float dt = (float)(toc-tic)/1.0e6;
   printf("Encoded %d bytes in %f s - %g bytes/s\n", size, dt, size/dt);
@@ -39,10 +41,13 @@ int main(){
   toc = av_gettime();
   dt = (float)(toc-tic)/1.0e6;
   printf("Decoded %d bytes in %f s - %g bytes/s\n", size, dt, size/dt);
-  if(memcmp(data, decoded, size)){
-    for(int i = 0;i<size;i++){
-      if(data[i] != decoded[i]){
-	printf("Bytes %d differ! %d and %d\n", i, data[i], decoded[i]);
+  if(memcmp(data, decoded, size))
+  {
+    for(int i = 0;i < size;i++)
+    {
+      if(data[i] != decoded[i])
+      {
+	      printf("Bytes %d differ! %d and %d\n", i, data[i], decoded[i]);
       }
     }
   }
